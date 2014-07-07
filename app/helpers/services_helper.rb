6ab99d7e59a05e8f54d51dea4ef6debd92095cc2
pdostal <git@pdostal.cz>
2014-07-07 07:16:42 +0200

    Implement ping

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,19 @@
 module ServicesHelper
+  require 'timeout'
+  require 'socket'
+
+  def ping(host, port)
+    begin
+      Timeout.timeout(5) do
+        s = TCPSocket.new(host, port)
+        s.close
+        return true
+      end
+    rescue Errno::ECONNREFUSED
+      return true
+    rescue Timeout::Error, Errno::ENETUNREACH, Errno::EHOSTUNREACH
+      return false
+    end
+  end
+
 end
