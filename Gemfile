1ce297d8b9ef2f4d4ac914da0268b2489c5faedd
pdostal <git@pdostal.cz>
2014-07-07 05:16:31 +0200

    Prepare controller

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
+gem 'bootstrap-sass', '~> 3.1.1'
+gem 'font-awesome-sass'
+
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
@@ -37,4 +40,5 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
 
---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,64 @@
+class ServicesController < ApplicationController
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
+
+  # GET /services
+  def index
+    @services = Service.all
+  end
+
+  # GET /services/1
+  def show
+  end
+
+  # GET /services/new
+  def new
+    @service = Service.new
+  end
+
+  # GET /services/1/edit
+  def edit
+  end
+
+  # POST /services
+  def create
+    @service = Service.new(service_params)
+
+    respond_to do |format|
+      if @service.save
+        format.html { redirect_to @service, notice: 'Service was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /services/1
+  def update
+    respond_to do |format|
+      if @service.update(service_params)
+        format.html { redirect_to @service, notice: 'Service was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /services/1
+  def destroy
+    @service.destroy
+    respond_to do |format|
+      format.html { redirect_to services_url, notice: 'Service was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_service
+      @service = Service.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def service_params
+      params.require(:service).permit(:name, :address, :port)
+    end
+end
