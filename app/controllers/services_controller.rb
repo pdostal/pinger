e5a4bc60a3718b3ae5c5115332a763d07f9cf5ec
pdostal <git@pdostal.cz>
2014-07-08 12:45:06 +0200

    Repair ordiring

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,13 +6,8 @@ class ServicesController < ApplicationController
     @services = Service.order(position: :asc)
   end
 
-  # POST /services/move
+  # POST /services/movedown
   def movedown
-    redirect_to services_url
-  end
-
-  # POST /services/moveup
-  def moveup
     actual = Service.where(id: params[:id]).first
     if actual.present?
       original = Service.where(position: actual.position.to_i + 1).first
@@ -25,8 +20,8 @@ class ServicesController < ApplicationController
     redirect_to services_url
   end
 
-  # POST /services/movedown
-  def movedown
+  # POST /services/moveup
+  def moveup
     actual = Service.where(id: params[:id]).first
     if actual.present?
       original = Service.where(position: actual.position.to_i - 1).first
@@ -41,6 +36,11 @@ class ServicesController < ApplicationController
 
   # GET /services/new
   def new
+    if Service.last.present?
+      @position = Service.last.position + 1
+    else
+      @position = 1
+    end
     @service = Service.new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'services#index'
 
   resources :services
-  post 'services/move' => 'services#move', :as => :services_move
   get 'services/moveup/:id' => 'services#moveup', :as => :services_moveup
   get 'services/movedown/:id' => 'services#movedown', :as => :services_movedown
 end
