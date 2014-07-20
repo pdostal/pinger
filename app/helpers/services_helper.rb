3a6226ad69bf91f188802c3f160aafd1b57d5c0a
pdostal <git@pdostal.cz>
2014-07-20 09:19:02 +0200

    Implement delay counter

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -2,17 +2,23 @@ module ServicesHelper
   require 'timeout'
   require 'socket'
 
+  def to_milliseconds time
+    return ( time * 1000 ).round(0)
+  end
+
   def ping(host, port)
     begin
       Timeout.timeout(5) do
+        beginning_time = Time.now
         s = TCPSocket.new(host, port)
         s.close
-        return true
+        end_time = Time.now
+        return to_milliseconds( end_time - beginning_time )
       end
     rescue Errno::ECONNREFUSED
-      return true
+      return to_milliseconds( end_time - beginning_time )
     rescue Timeout::Error, Errno::ENETUNREACH, Errno::EHOSTUNREACH
-      return false
+      return 0
     end
   end
 
