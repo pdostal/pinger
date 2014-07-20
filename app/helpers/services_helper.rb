200762fc7ca3e848ac3849908563fccfe1abd54c
pdostal <git@pdostal.cz>
2014-07-20 10:51:18 +0200

    Implement Ping:external

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -6,19 +6,32 @@ module ServicesHelper
     return ( time * 1000 ).round(0)
   end
 
-  def ping(host, port)
-    begin
-      Timeout.timeout(5) do
-        beginning_time = Time.now
-        s = TCPSocket.new(host, port)
-        s.close
-        end_time = Time.now
+  def ping host, port = nil
+    if port.nil?
+      require 'net/ping'
+      beginning_time = Time.now
+      p = Net::Ping::External.new(host)
+      p = p.ping?
+      end_time = Time.now
+      if p == true
         return to_milliseconds( end_time - beginning_time )
+      else
+        return 0
       end
-    rescue Errno::ECONNREFUSED
-      return to_milliseconds( end_time - beginning_time )
-    rescue Timeout::Error, Errno::ENETUNREACH, Errno::EHOSTUNREACH
-      return 0
+    else
+      begin
+        Timeout.timeout(5) do
+          beginning_time = Time.now
+          s = TCPSocket.new(host, port)
+          s.close
+          end_time = Time.now
+          return to_milliseconds( end_time - beginning_time )
+        end
+      rescue Errno::ECONNREFUSED
+        return to_milliseconds( end_time - beginning_time )
+      rescue Timeout::Error, Errno::ENETUNREACH, Errno::EHOSTUNREACH
+        return 0
+      end
     end
   end
 
